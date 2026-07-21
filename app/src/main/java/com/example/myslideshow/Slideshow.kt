a581f79cf21379a2faa44c98b572e07630febb1a
itc-s24016 <s24016@std.it-college.ac.jp>
2026-07-21 16:08:59 +0900

    自動スクロール対応まで

--- app/src/main/java/com/example/myslideshow/Slideshow.kt
+++ app/src/main/java/com/example/myslideshow/Slideshow.kt
@@ -9,10 +9,16 @@ import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 val resources = listOf(
     R.drawable.slide00, R.drawable.slide01, R.drawable.slide02,
@@ -25,11 +31,21 @@ val resources = listOf(
 @Composable
 fun Slideshow(){
     val pagerState = rememberPagerState(pageCount = {resources.size})
-    val config = LocalConfiguration.current
+    val config: Configuration = LocalConfiguration.current
+    var slideshow by remember { mutableStateOf(false) }
+
+    LaunchedEffect(slideshow) {
+        while (slideshow){
+            delay(3000)
+            pagerState.animateScrollToPage(
+                page = (pagerState.currentPage + 1) % resources.size
+            )
+        }
+    }
     Scaffold(
         topBar = {
             if (config.orientation != Configuration.ORIENTATION_LANDSCAPE){
-                TopBar()
+                TopBar(isRunning = slideshow){slideshow = !slideshow}
             }
         },
         bottomBar = {
